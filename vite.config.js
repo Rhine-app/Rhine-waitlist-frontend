aff57c163c6dadb73091cb645ae5df040468228a
157994689+Sechorda@users.noreply.github.com <157994689+Sechorda@users.noreply.github.com>
2025-04-01 15:38:43 -0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/waitlist/',
+  base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js'
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',  // Explicitly set to 0.0.0.0 instead of true
     port: 3000,
